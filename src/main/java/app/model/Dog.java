735b07f81b40d5c3c359313a7673331bf84d5a64
grrowerr <grouw.uzkiy@yandex.ru>
2024-11-04 01:22:02 +0300

    add class Dog, update Timer, update AnimalsCage, check Tests.

---
+++ src/main/java/app/model/Dog.java
@@ -0,0 +1,9 @@
+package app.model;
+
+
+import org.springframework.stereotype.Component;
+
+@Component("dogBean")
+public class Dog extends Animal {
+
+}
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,5 +1,9 @@
 package app.model;
 
+
+import org.springframework.stereotype.Component;
+
+@Component("timerBean")
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
